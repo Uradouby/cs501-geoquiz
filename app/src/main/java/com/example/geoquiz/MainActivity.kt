19e249ce5e5b6a78629a7e2749034700254625db
Uradouby <1468247858@qq.com>
2023-01-30 21:45:44 -0500

    finish geoquiz with snackbar

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,18 +19,18 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
+            Snackbar.make(
                 this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+                view,
+                getString(R.string.correct_toast),
+                BaseTransientBottomBar.LENGTH_SHORT).show()
         }
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
+            Snackbar.make(
                 this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+                view,
+                getString(R.string.incorrect_toast),
+                BaseTransientBottomBar.LENGTH_SHORT).show()
         }
     }
 }
